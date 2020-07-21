47da0f479366514721f2fc1078eb536f32bc9531
Max Katz <maxpkatz@gmail.com>
2020-07-21 10:45:34 -0700

    Spacing

--- exercises/hw7/multi_solution.cu
+++ exercises/hw7/multi_solution.cu
@@ -14,27 +14,29 @@ const int num_gpus = 4;
 // not modifiable
 const float sqrt_2PIf = 2.5066282747946493232942230134974f;
 const double sqrt_2PI = 2.5066282747946493232942230134974;
-__device__ float gpdf(float val, float sigma){
-  return expf(-0.5f * val * val)/(sigma*sqrt_2PIf);
+__device__ float gpdf(float val, float sigma) {
+  return expf(-0.5f * val * val) / (sigma * sqrt_2PIf);
 }
 
-__device__ double gpdf(double val, double sigma){
-  return exp(-0.5 * val * val)/(sigma*sqrt_2PI);
+__device__ double gpdf(double val, double sigma) {
+  return exp(-0.5 * val * val) / (sigma * sqrt_2PI);
 }
 
 // compute average gaussian pdf value over a window around each point
-__global__ void gaussian_pdf(const ft * __restrict__ x, ft * __restrict__ y, const ft mean, const ft sigma, const int n){
-  int idx = threadIdx.x+blockDim.x*blockIdx.x;
-  if (idx < n){
-    ft in = x[idx] - (count/2)*0.01f;
+__global__ void gaussian_pdf(const ft * __restrict__ x, ft * __restrict__ y, const ft mean, const ft sigma, const int n) {
+  int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  if (idx < n) {
+    ft in = x[idx] - (count / 2) * 0.01f;
     ft out = 0;
-    for (int i = 0; i < count; i++){
-      ft temp = (in - mean)/sigma;
+    for (int i = 0; i < count; i++) {
+      ft temp = (in - mean) / sigma;
       out += gpdf(temp, sigma);
-      in += 0.01f;}
-    y[idx] = out/count;
+      in += 0.01f;
     }
+    y[idx] = out / count;
+  }
 }
+
 // error check macro
 #define cudaCheckErrors(msg) \
   do { \
@@ -51,33 +53,43 @@ __global__ void gaussian_pdf(const ft * __restrict__ x, ft * __restrict__ y, con
 // host-based timing
 #define USECPSEC 1000000ULL
 
-unsigned long long dtime_usec(unsigned long long start){
-
+unsigned long long dtime_usec(unsigned long long start) {
   timeval tv;
   gettimeofday(&tv, 0);
   return ((tv.tv_sec*USECPSEC)+tv.tv_usec)-start;
 }
 
-int main(){
+int main() {
   ft *h_x, *d_x[num_gpus], *d_y[num_gpus];
-  h_x = (ft *)malloc(ds*sizeof(ft));
-  for (int i = 0; i < num_gpus; i++){
+  h_x = (ft *)malloc(ds * sizeof(ft));
+
+  for (int i = 0; i < num_gpus; i++) {
     cudaSetDevice(i);
-    cudaMalloc(&d_x[i], ds*sizeof(ft));
-    cudaMalloc(&d_y[i], ds*sizeof(ft));}
+    cudaMalloc(&d_x[i], ds * sizeof(ft));
+    cudaMalloc(&d_y[i], ds * sizeof(ft));
+  }
   cudaCheckErrors("allocation error");
-  for (int i = 0; i < num_gpus; i++){
-    for (size_t j = 0; j < ds; j++) h_x[j] = rand()/(ft)RAND_MAX;
+
+  for (int i = 0; i < num_gpus; i++) {
+    for (size_t j = 0; j < ds; j++) {
+      h_x[j] = rand() / (ft)RAND_MAX;
+    }
     cudaSetDevice(i);
-    cudaMemcpy(d_x[i], h_x, ds*sizeof(ft), cudaMemcpyHostToDevice);}
+    cudaMemcpy(d_x[i], h_x, ds * sizeof(ft), cudaMemcpyHostToDevice);
+  }
   cudaCheckErrors("copy error");
+
   unsigned long long et1 = dtime_usec(0);
-  for (int i = 0; i < num_gpus; i++){
+
+  for (int i = 0; i < num_gpus; i++) {
     cudaSetDevice(i);
-    gaussian_pdf<<<(ds+255)/256, 256>>>(d_x[i], d_y[i], 0.0, 1.0, ds);}
+    gaussian_pdf<<<(ds+255)/256, 256>>>(d_x[i], d_y[i], 0.0, 1.0, ds);
+  }
   cudaDeviceSynchronize();
-  et1 = dtime_usec(et1);
   cudaCheckErrors("execution error");
+
+  et1 = dtime_usec(et1);
   std::cout << "elapsed time: " << et1/(float)USECPSEC << std::endl;
+
   return 0;
 }
--- exercises/hw7/overlap.cu
+++ exercises/hw7/overlap.cu
@@ -14,27 +14,29 @@ const int num_streams = 8;
 // not modifiable
 const float sqrt_2PIf = 2.5066282747946493232942230134974f;
 const double sqrt_2PI = 2.5066282747946493232942230134974;
-__device__ float gpdf(float val, float sigma){
-  return expf(-0.5f * val * val)/(sigma*sqrt_2PIf);
+__device__ float gpdf(float val, float sigma) {
+  return expf(-0.5f * val * val) / (sigma * sqrt_2PIf);
 }
 
-__device__ double gpdf(double val, double sigma){
-  return exp(-0.5 * val * val)/(sigma*sqrt_2PI);
+__device__ double gpdf(double val, double sigma) {
+  return exp(-0.5 * val * val) / (sigma * sqrt_2PI);
 }
 
 // compute average gaussian pdf value over a window around each point
-__global__ void gaussian_pdf(const ft * __restrict__ x, ft * __restrict__ y, const ft mean, const ft sigma, const int n){
-  int idx = threadIdx.x+blockDim.x*blockIdx.x;
-  if (idx < n){
-    ft in = x[idx] - (count/2)*0.01f;
+__global__ void gaussian_pdf(const ft * __restrict__ x, ft * __restrict__ y, const ft mean, const ft sigma, const int n) {
+  int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  if (idx < n) {
+    ft in = x[idx] - (count / 2) * 0.01f;
     ft out = 0;
-    for (int i = 0; i < count; i++){
-      ft temp = (in - mean)/sigma;
+    for (int i = 0; i < count; i++) {
+      ft temp = (in - mean) / sigma;
       out += gpdf(temp, sigma);
-      in += 0.01f;}
-    y[idx] = out/count;
+      in += 0.01f;
     }
+    y[idx] = out / count;
+  }
 }
+
 // error check macro
 #define cudaCheckErrors(msg) \
   do { \
@@ -51,14 +53,13 @@ __global__ void gaussian_pdf(const ft * __restrict__ x, ft * __restrict__ y, con
 // host-based timing
 #define USECPSEC 1000000ULL
 
-unsigned long long dtime_usec(unsigned long long start){
-
+unsigned long long dtime_usec(unsigned long long start) {
   timeval tv;
   gettimeofday(&tv, 0);
   return ((tv.tv_sec*USECPSEC)+tv.tv_usec)-start;
 }
 
-int main(){
+int main() {
   ft *h_x, *d_x, *h_y, *h_y1, *d_y;
   cudaHostAlloc(&h_x,  ds*sizeof(ft), cudaHostAllocDefault);
   cudaHostAlloc(&h_y,  ds*sizeof(ft), cudaHostAllocDefault);
@@ -66,32 +67,54 @@ int main(){
   cudaMalloc(&d_x, ds*sizeof(ft));
   cudaMalloc(&d_y, ds*sizeof(ft));
   cudaCheckErrors("allocation error");
+
   cudaStream_t streams[num_streams];
-  for (int i = 0; i < num_streams; i++) cudaStreamCreate(&streams[i]);
+  for (int i = 0; i < num_streams; i++) {
+    cudaStreamCreate(&streams[i]);
+  }
   cudaCheckErrors("stream creation error");
-  gaussian_pdf<<<(ds+255)/256, 256>>>(d_x, d_y, 0.0, 1.0, ds); // warmp-up
-  for (size_t i = 0; i < ds; i++) h_x[i] = rand()/(ft)RAND_MAX;
+
+  gaussian_pdf<<<(ds + 255) / 256, 256>>>(d_x, d_y, 0.0, 1.0, ds); // warm-up
+
+  for (size_t i = 0; i < ds; i++) {
+    h_x[i] = rand() / (ft)RAND_MAX;
+  }
   cudaDeviceSynchronize();
+
   unsigned long long et1 = dtime_usec(0);
-  cudaMemcpy(d_x, h_x, ds*sizeof(ft), cudaMemcpyHostToDevice);
-  gaussian_pdf<<<(ds+255)/256, 256>>>(d_x, d_y, 0.0, 1.0, ds);
-  cudaMemcpy(h_y1, d_y, ds*sizeof(ft), cudaMemcpyDeviceToHost);
-  et1 = dtime_usec(et1);
+
+  cudaMemcpy(d_x, h_x, ds * sizeof(ft), cudaMemcpyHostToDevice);
+  gaussian_pdf<<<(ds + 255) / 256, 256>>>(d_x, d_y, 0.0, 1.0, ds);
+  cudaMemcpy(h_y1, d_y, ds * sizeof(ft), cudaMemcpyDeviceToHost);
   cudaCheckErrors("non-streams execution error");
+
+  et1 = dtime_usec(et1);
   std::cout << "non-stream elapsed time: " << et1/(float)USECPSEC << std::endl;
+
 #ifdef USE_STREAMS
-  cudaMemset(d_y, 0, ds*sizeof(ft));
+  cudaMemset(d_y, 0, ds * sizeof(ft));
+
   unsigned long long et = dtime_usec(0);
-  for (int i = 0; i < chunks; i++){ //depth-first launch
-    cudaMemcpyAsync(d_x+FIXME, h_x+FIXME, (FIXME)*sizeof(ft), cudaMemcpyHostToDevice, streams[FIXME]);
-    gaussian_pdf<<<((FIXME)+255)/256, 256,0,streams[FIXME]>>>(d_x+FIXME, d_y+FIXME, 0.0, 1.0, FIXME);
-    cudaMemcpyAsync(h_y+i*(ds/chunks), d_y+i*(ds/chunks), (ds/chunks)*sizeof(ft), cudaMemcpyDeviceToHost, streams[i%num_streams]);
-    }
+
+  for (int i = 0; i < chunks; i++) { //depth-first launch
+    cudaMemcpyAsync(d_x + FIXME, h_x + FIXME, (FIXME) * sizeof(ft), cudaMemcpyHostToDevice, streams[FIXME]);
+    gaussian_pdf<<<((FIXME) + 255) / 256, 256, 0, streams[FIXME]>>>(d_x + FIXME, d_y + FIXME, 0.0, 1.0, FIXME);
+    cudaMemcpyAsync(h_y + i * (ds / chunks), d_y + i * (ds / chunks), (ds / chunks) * sizeof(ft), cudaMemcpyDeviceToHost, streams[i % num_streams]);
+  }
   cudaDeviceSynchronize();
-  et = dtime_usec(et);
   cudaCheckErrors("streams execution error");
-  for (int i = 0; i < ds; i++) if (h_y[i] != h_y1[i]) {std::cout << "mismatch at " << i << " was: " << h_y[i] << " should be: " << h_y1[i] << std::endl; return -1;}
+
+  et = dtime_usec(et);
+
+  for (int i = 0; i < ds; i++) {
+    if (h_y[i] != h_y1[i]) {
+      std::cout << "mismatch at " << i << " was: " << h_y[i] << " should be: " << h_y1[i] << std::endl;
+      return -1;
+    }
+  }
+
   std::cout << "streams elapsed time: " << et/(float)USECPSEC << std::endl;
 #endif
+
   return 0;
 }
